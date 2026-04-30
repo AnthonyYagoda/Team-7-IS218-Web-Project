1bf9d0c53948a083860250e55411e565e8a15d34
sms325-ux <sms325@njit.edu>
2026-04-30 14:48:22 -0400

    -Updated the tabs in the navbar (Drink List, Drink Information) -Changed the text of the home page -Changed the about and contact html files to be drink list and drink information respectively -Adjusted urls in the urls.py and views.py file to display the tab names -Created new content for the drink list and drink information pages

--- hello/urls.py
+++ hello/urls.py
@@ -5,6 +5,6 @@ from hello import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("hello/<str:name>/", views.hello_there, name="hello_there"), # Lab 2 - Add a URL pattern for the hello_there view that captures a string parameter called name
-    path("about/", views.about, name="about"), # Lab 2 - Add a URL pattern for the about view
-    path("contact/", views.contact, name="contact"), # Lab 2 - Add a URL pattern for the contact view
+    path("DrinkList/", views.DrinkList, name="DrinkList"), # Lab 2 - Add a URL pattern for the about view
+    path("DrinkInformation/", views.DrinkInformation, name="DrinkInformation"), # Lab 2 - Add a URL pattern for the contact view
 ]
